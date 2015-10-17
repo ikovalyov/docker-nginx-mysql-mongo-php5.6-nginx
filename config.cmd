49c55f123ea8208aacf09ffc8764cb959df335f7
Ilya Kovalyov <ikovalyov85@gmail.com>
2015-10-17 16:15:09 +0300

    php and ssh

---
+++ config.cmd
@@ -0,0 +1,12 @@
+docker-machine start dev
+docker-machine ssh dev "mkdir /home/docker/docker"
+docker-machine ssh dev "mkdir /home/docker/documentRoot"
+docker-machine ssh dev "sudo mount -t vboxsf -o rw,uid=1000,gid=1000 /home/docker56 /home/docker/docker"
+docker-machine ssh dev "sudo mount -t vboxsf -o rw,uid=1000,gid=1000 /documentRoot /home/docker/documentRoot"
+docker-machine ssh dev "docker build -t ikovalyov/nginx:latest /home/docker/docker/nginx/"
+docker-machine ssh dev "docker build -t ikovalyov/php-xdebug:latest /home/docker/docker/php-fpm/"
+docker-machine ssh dev "docker run -dt --name=websites -v /home/docker/documentRoot://opt/webapp ubuntu"
+docker-machine ssh dev "docker run -p 27017:27017 --name mongo -d mongo"
+docker-machine ssh dev "docker run -p 3306:3306 --name mysql -e MYSQL_ROOT_PASSWORD=rootPass -d mysql:5.7"
+docker-machine ssh dev "docker run -dt -p 9000:9000 -p 9001:9001 -p 10022:22 --name=php-xdebug --volumes-from websites --link mongo --link mysql ikovalyov/php-xdebug"
+docker-machine ssh dev "docker run -dt -p 80:80 -p 443:443 --name=nginx --volumes-from websites --link php-xdebug:php ikovalyov/nginx"
--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
-service php7-fpm start; 
-service mysql start;
-nginx -g "daemon off;";
-mongod --fork --logpath /var/log/mongod.log;
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES"
-bash
+docker-machine start dev
+docker-machine ssh dev "mkdir /home/docker/docker"
+docker-machine ssh dev "mkdir /home/docker/documentRoot"
+docker-machine ssh dev "sudo mount -t vboxsf -o rw,uid=1000,gid=1000 /home/docker56 /home/docker/docker"
+docker-machine ssh dev "sudo mount -t vboxsf -o rw,uid=1000,gid=1000 /documentRoot /home/docker/documentRoot"
+docker-machine ssh dev "docker start websites"
+docker-machine ssh dev "docker start mongo"
+docker-machine ssh dev "docker start mysql"
+docker-machine ssh dev "docker start php-xdebug"
+docker-machine ssh dev "docker start nginx"
